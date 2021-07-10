ae9d9febfb564870fc062158379dcfbddce3870d
814657780 <18037818812@163.com>
2021-07-10 21:46:03 +0800

    嵌套路由

--- vue-router/2、嵌套路由/router2/src/router/index.js
+++ vue-router/2、嵌套路由/router2/src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,7 +15,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue'),
+    children: [{
+      path: 'child',
+      name: 'About-child',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "about-child" */ '../views/About-child.vue'),
+    }]
   }
 ]
 
--- vue-router/2、嵌套路由/router2/src/views/About.vue
+++ vue-router/2、嵌套路由/router2/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link to="/about/child">加载子页面</router-link>
+    <router-view></router-view>
   </div>
 </template>
