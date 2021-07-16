63c7a32c0edada7c4eb359a9565d32951f2b3998
814657780 <18037818812@163.com>
2021-07-16 23:17:42 +0800

    7、路由组件传参

---
+++ vue-router/7、路由组件传参/router7/src/components/child1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="child1">
+     <p>这是child1页面</p>
+     <p>user {{id}}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+    props:['id']
+
+}
+</script>
+
+<style>
+
+</style>
--- vue-router/7、路由组件传参/router7/src/views/About.vue
+++ vue-router/7、路由组件传参/router7/src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link :to="{name:'child1'}">go to child1</router-link></br>
+    <router-link :to="{name:'child1',params:{id:'1234'}}">go to child1</router-link>
+    <router-view></router-view>
   </div>
 </template>
