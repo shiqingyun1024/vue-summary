e8963e9b7f1ea80d77b9d0af7128e0d9ed6adf8d
814657780 <18037818812@163.com>
2021-07-17 23:36:58 +0800

    8、HTML5 History

---
+++ vue-router/8、HTML5 History模式/hash/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    window.onload = function() {
+      // hash变化时触发
+      window.onhashchange = function(opt) {
+        console.log(opt);
+      };
+    };
+  },
+  watch:{
+    $route(newValue,oldValue){
+      window.onhashchange = function(opt) {
+        console.log("hash值的变化");
+        console.log(opt);
+      };
+      // 这里面
+      console.log(newValue);
+      console.log(oldValue);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue-router/8、HTML5 History模式/hash/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link :to="{name:'child1'}">去child1页面</router-link></br>
+    <router-link :to="{name:'child2'}">去child2页面</router-link>
+    <router-view></router-view>
+  </div>
+</template>
