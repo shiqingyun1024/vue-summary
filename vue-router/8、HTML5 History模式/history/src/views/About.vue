f39a9042e95bddca304d3a7f400193184af1575a
814657780 <18037818812@163.com>
2021-07-24 10:52:49 +0800

    HTML5 History模式

--- vue-router/8、HTML5 History模式/history/src/views/About.vue
+++ vue-router/8、HTML5 History模式/history/src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a href="#/123">123</a></br>
+    <a href="456">456</a></br>
   </div>
 </template>
+<script>
+
+export default {
+  created(){
+    console.log('1234');
+    window.addEventListener('hashchange',function(event){
+       console.log('url发生改变');
+    })
+    window.onhashchange = function(event){
+       console.log('url发生改变---onhashchange');
+    }
+    window.addEventListener('popstate',function(event){
+       console.log('前进或者后退');
+    })
+    window.onhashchange = function(opt) {
+        console.log(opt);
+      };
+    // };
+    window.addEventListener('hashchange',(option)=>{
+           console.log("hash值的变化");
+    })
+  }
+}
+</script>
--- vue-router/8、HTML5 History模式/history/src/views/Home.vue
+++ vue-router/8、HTML5 History模式/history/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="#/home12">home123</a>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
@@ -13,6 +14,25 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created(){
+    console.log('1234');
+    window.addEventListener('hashchange',function(event){
+       console.log('url发生改变');
+    })
+    window.onhashchange = function(event){
+       console.log('url发生改变---onhashchange');
+    }
+    window.addEventListener('popstate',function(event){
+       console.log('前进或者后退');
+    })
+    window.onhashchange = function(opt) {
+        console.log(opt);
+      };
+    // };
+    window.addEventListener('hashchange',(option)=>{
+           console.log("hash值的变化");
+    })
   }
 }
 </script>
