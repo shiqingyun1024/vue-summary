b3b9480252b7c0442c5b01ff545f2575b49d718f
814657780 <18037818812@163.com>
2021-07-24 15:32:58 +0800

    导航守卫

--- vue-router/进阶/1、导航守卫/router9/src/router/index.js
+++ vue-router/进阶/1、导航守卫/router9/src/router/index.js
@@ -16,14 +16,31 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    beforeEnter: (to, from, next) => {
+      console.log('路由独享的守卫--beforeEnter');
+      next();
+    }
   }
 ]
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+router.beforeEach((to,from,next)=>{
+  console.log('这是全局前置守卫--beforeEach');
+  next();
+})
+router.beforeResolve((to,from,next)=>{
+  console.log('这是全局解析守卫--beforeResolve');
+  next();
+})
+router.afterEach((to, from) => {
+  console.log('这是全局后置钩子--afterEach');
+})
+
 
 export default router
