7f821fc4d3d3795967471601daafe5280aa5ca9c
814657780 <18037818812@163.com>
2021-07-26 21:22:52 +0800

    keep-alive缓存动态组件

--- vue-router/keep-alive缓存路由组件/keep-alive缓存动态组件/router11/src/components/child2.vue
+++ vue-router/keep-alive缓存路由组件/keep-alive缓存动态组件/router11/src/components/child2.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="child2">
+
   </div>
 </template>
+
 <script>
 export default {
-  
+
 }
 </script>
+
+<style>
+
+</style>
