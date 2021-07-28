78d2a8f78358ead1a59a562d3b126e390e8bf611
814657780 <18037818812@163.com>
2021-07-28 21:41:29 +0800

    vuex中的state

--- vuex/state/vuex2/src/store/index.js
+++ vuex/state/vuex2/src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count:1
   },
   mutations: {
+    increment(state){
+      state.count++
+    }
   },
   actions: {
   },
--- vuex/state/vuex2/src/views/About.vue
+++ vuex/state/vuex2/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>count:{{count}}</p>
   </div>
 </template>
+<script>
+export default {
+  name:'about',
+  data(){
+    return{
+
+    }
+  },
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  }
+}
+</script>
