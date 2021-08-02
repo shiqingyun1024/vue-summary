e4123920a4371eb612975be0f75f29ec73873141
814657780 <18037818812@163.com>
2021-08-02 22:49:55 +0800

    vue 中 hook的使用

--- 功能模块/6、vue hook 使用/vue-hook/src/views/About.vue
+++ 功能模块/6、vue hook 使用/vue-hook/src/views/About.vue
@@ -3,3 +3,27 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+export default {
+  name:'about',
+  data(){
+    return{
+      timer:null
+    }
+  },
+  mounted(){
+    // 会一直执行下去
+    // let timer2 = setInterval(()=>{
+    //   console.log('timer2');
+    // },2000)
+    this.timer = setInterval(()=>{
+      console.log('timer');
+    },2000)
+    this.$once('hook:beforeDestroy',()=>{
+      console.log('停止');
+      clearInterval(this.timer);
+      this.timer = null;
+    })
+  }
+}
+</script>
