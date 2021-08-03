629f79d53d651158885b5ed07e6a2fedcf2aca53
WINDOWS-289P217\EDZ <18037818812@163.com>
2021-08-03 18:19:32 +0800

    v-model原理的理解

--- 功能模块/7、v-model的理解/vue-model/src/views/About.vue
+++ 功能模块/7、v-model的理解/vue-model/src/views/About.vue
@@ -1,31 +1,74 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <input type="text" v-model="username">
-    <p>{{username}}</p>
+    <input type="text" v-model="username" />
+    <p>{{ username }}</p>
     <!-- v-model等同于下面 :value和@input，是一个语法糖-->
-    <br>
+    <br />
     <!-- type为text -->
-    <input type="text" :value="username2" @input="username2=$event.target.value">
-    <p>{{username2}}</p>
-     <!-- type为checkbox -->
-     <input type="checkbox" id="checkbox" v-model="checked">
-     <label for="checkbox">{{ checked }}</label>
-     <br>
-     <input type="checkbox" id="checkbox" :value="checked2" @change="checked2=$event.target.value">
-     <label for="checkbox">{{ checked2 }}</label>
+    <input
+      type="text"
+      :value="username2"
+      @input="username2 = $event.target.value"
+    />
+    <p>{{ username2 }}</p>
+    <!-- type为checkbox -->
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">{{ checked }}</label>
+    <br />
+    <br />
+    <input type="checkbox" id="checkbox2" :value="checked2" @change="change" />
+    <label for="checkbox2">{{ checked2 }}</label>
+    <br />
+    <br />
+    <!-- v-model="checkedNames" -->
+    <!-- 在type="radio"上 v-model等于  @change="checkedNames=$event.target.value"-->
+    <input
+      type="radio"
+      id="jack"
+      value="Jack"
+      @change="checkedName = $event.target.value"
+    />
+    <label for="jack">Jack</label>
+    <input type="radio" id="john" value="John" v-model="checkedName" />
+    <label for="john">John</label>
+    <input type="radio" id="mike" value="Mike" v-model="checkedNam" />
+    <label for="mike">Mike</label>
+    <br />
+    <br />
+    <span>Checked names: {{ checkedName }}</span>
+    <br />
+    <br />
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+    <br />
+    <span>Checked names: {{ checkedNames }}</span>
   </div>
 </template>
 <script>
 export default {
-  name:'about',
-  data(){
+  name: "about",
+  data() {
     return {
-      username:'',
-      username2:'',
-      checked:false,
-      checked2:false,
-    }
-  }
-}
+      username: "",
+      username2: "",
+      checked: false,
+      checked2: false,
+      checkedName: "",
+      checkedNames: [],
+    };
+  },
+  methods: {
+    // checked2=$event.target.value
+    change(e) {
+      {
+        console.log(e.target.value);
+      }
+    },
+  },
+};
 </script>
