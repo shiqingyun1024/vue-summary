9cc13366f8c0eb5bb6ee6f7f793799e4696a4d6d
814657780 <18037818812@163.com>
2021-08-18 07:39:00 +0800

    计算属性

---
+++ vue基础/计算属性和侦听器/computed-and-watcher/src/components/tab-bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <label for=""><input type="radio" ></label>
+      
+  </div>
+</template>
+
+<script>
+export default {
+   name:'TabBar',
+   props:['data']
+}
+</script>
+
+<style>
+
+</style>
--- vue基础/计算属性和侦听器/computed-and-watcher/src/views/About.vue
+++ vue基础/计算属性和侦听器/computed-and-watcher/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input type="text" v-model="message">
+    <h2>message:{{message}}</h2>
+    <hr>
+    <tab-bar :data="tabs" v-model="selectedLabel">
+    </tab-bar>
+    <h3>当前选项：{{selectedLabel}}</h3>
   </div>
 </template>
+<script>
+import TabBar from '@/components/tab-bar.vue'
+export default {
+  components: { TabBar },
+  name:'about',
+  data(){
+    return {
+       message:'about页面',
+       index:0,
+       tabs:[
+         {label:'选项1'},
+         {label:'选项2'},
+         {label:'选项3'}
+       ]
+    }
+  },
+  computed:{
+    selectedLabel(){
+      return this.tabs[this.index].label
+    }
+  }
+}
+</script>
