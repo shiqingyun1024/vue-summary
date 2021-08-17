eb77a21baf8c37266a0d91fef7cf22da3555ba14
814657780 <18037818812@163.com>
2021-08-17 22:11:25 +0800

    模板语法

---
+++ vue基础/模板语法/template-syntax/src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p v-once>{{name}}</p>
+    <button @click="changeName">changeName</button>
+  </div>
+</template>
+<script>
+export default {
+   name:'about',
+   data(){
+     return {
+       name:'about页面'
+     }
+   },
+   methods:{
+     changeName(){
+       this.name = '改变name的值'
+     }
+   }
+}
+</script>
