3907e0af4a32d39dff8dbaa455533e112d314685
xiaolin.shi <xiaolin.shi@riskeys.com>
2021-08-31 18:16:40 +0800

    Vue.use

--- 功能模块/8、vue.use的理解/vue-use/src/main.js
+++ 功能模块/8、vue.use的理解/vue-use/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import useCom from '@/untils/use'
-import useCom2 from '@/untils/use2'
+import useCom from '@/plugins/use'
+import useCom2 from '@/plugins/use2'
 Vue.use(useCom,'参数')
 Vue.use(useCom2,'参数2')
 
