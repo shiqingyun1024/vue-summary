fb681af14bee69f93de9437e27657bcd1c2f5e37
814657780 <18037818812@163.com>
2021-09-17 22:35:44 +0800

    手写双向数据绑定原理

--- 功能模块/11、双向数据绑定原理/vue.js
+++ 功能模块/11、双向数据绑定原理/vue.js
@@ -2,16 +2,20 @@ export default class Vue{
     constructor(options){
         this.$el = document.querySelector(options.el);
         this.$data = options.data;
-        this.compile();
+        this.compile(this.$el);
     }
-    compile(){
-        console.log(this.$el.childNodes);
-        console.log(this.$el);
-        this.$el.childNodes.forEach(element => {
-            if(element.nodeType== '1'){
+    compile(node){
+        node.childNodes.forEach(element => {
+            if(element.nodeType== '1'&&element.childNodes.length>0){  // nodeType等于3是元素节点
                 this.compile(element);
-            }else if(element.nodeType== '3'){
-                element.replace(/\{\{(.*?)\}\}/,match())
+            }else if(element.nodeType== '3'){  // nodeType等于3是文本节点
+                let reg = /\{\{(.*?)\}\}/g;
+                let text = element.textContent;
+                element.textContent = text.replace(reg,(match,vmKey)=>{
+                    let key = vmKey.trim()
+                    return this.$data[key]
+                    //  console.log(match,vmKey);
+                })
             }
         });
     }
