d86aedc9b29dbf93ef72441dd5ac113c23c2123c
814657780 <18037818812@163.com>
2021-09-17 22:54:55 +0800

    手写双向数据绑定原理

--- 功能模块/11、双向数据绑定原理/vue.js
+++ 功能模块/11、双向数据绑定原理/vue.js
@@ -2,15 +2,23 @@ export default class Vue{
     constructor(options){
         this.$el = document.querySelector(options.el);
         this.$data = options.data;
+        this.$methods = options.methods;
         this.compile(this.$el);
     }
     compile(node){
         node.childNodes.forEach(element => {
-            if(element.nodeType== '1'&&element.childNodes.length>0){  // nodeType等于3是元素节点
-                this.compile(element);
+            if(element.nodeType== '1'){  // nodeType等于3是元素节点
+                if(element.hasAttribute('@click')){
+                    let key = element.getAttribute('@click')
+                    element.addEventListener('click',this.$methods[key])
+                }
+                if(element.childNodes.length>0){
+                    this.compile(element);
+                }
             }else if(element.nodeType== '3'){  // nodeType等于3是文本节点
                 let reg = /\{\{(.*?)\}\}/g;
                 let text = element.textContent;
+                // 这一块的replace的用法很独特，很值得借鉴。match就相当于匹配的值{{str}}，vmKey就是匹配中的str，其实主要是获取str
                 element.textContent = text.replace(reg,(match,vmKey)=>{
                     let key = vmKey.trim()
                     return this.$data[key]
