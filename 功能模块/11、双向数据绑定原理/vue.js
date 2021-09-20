181d40004429a6d925cbd1e3181e5460ee829c31
814657780 <18037818812@163.com>
2021-09-20 22:38:01 +0800

    双向数据绑定原理

--- 功能模块/11、双向数据绑定原理/vue.js
+++ 功能模块/11、双向数据绑定原理/vue.js
@@ -119,6 +119,7 @@ class Watcher{
 *     更新函数，添加监听数据的订阅这，一旦数据有变动，受到通知，更新视图。
 *  3、Watcher订阅者是Observer和Compile之间通信的桥梁，主要做的事情是：(1)、在自身实例化时往属性订阅器(dep)里面添加自己。
 *    (2)、自身必须有一个update()方法。(3)、待属性变动dep.notice()通知时，能调用自身的update()方法，并触发Compile中绑定的回调，则功成身退。
-*  4、MVVM
+*  4、MVVM作为数据绑定的入口，整合Observer、Compile和Watcher三者，通过Oberver来监听自己的model数据变化，通过Complie来解析编译模板指令，最终利用
+*     Watcher搭起Observer和Compile之间的通信桥梁，达到数据变化->视图更新；视图交互变化（input）->数据model变更的双向绑定效果。
 *
 */
